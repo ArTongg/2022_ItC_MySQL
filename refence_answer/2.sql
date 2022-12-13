694269262f2f18177df07cb43452dbbc07f69aa9
ArTongg <117624829+ArTongg@users.noreply.github.com>
2022-12-13 22:44:06 +0800

    Delete refence_answer directory

--- refence_answer/2.sql
+++
@@ -1,9 +0,0 @@
--- 2. Find all characters that never visited any empire planets. 
-
-SELECT DISTINCT C.Name 
-    FROM Characters C 
-    WHERE NOT EXISTS (
-        SELECT * 
-        FROM Planets P, TimeTable T 
-        WHERE T.Planet_Name = P.Name AND P.Affiliation = 'empire' AND C.Name = T.Character_Name
-    );
--- refence_answer/5.sql
+++
@@ -1,7 +0,0 @@
--- 5. Which planet(s) have not been visited by any characters in all movies?
-
-SELECT DISTINCT P.Name 
-    FROM Planets P 
-    WHERE P.Name NOT IN (
-        SELECT DISTINCT Planet_Name FROM TimeTable
-    );
